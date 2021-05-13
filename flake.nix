e8f973e988e16a4c07eb01f0b23ecd7590f689d4
nokx <72279573+nokx5@users.noreply.github.com>
2021-05-14 00:58:27 +0200

    fix compiler issue

--- flake.nix
+++ flake.nix
@@ -13,17 +13,21 @@
           inherit system;
           config = { allowUnfree = true; };
         };
-	
-        golden_cpp = (with pkgs;
-	  stdenv.mkDerivation rec {
+
+        golden_cpp = 
+	(with pkgs;
+	let
+	stdEnv=clangStdenv; # gccStdenv # clangStdenv
+	in
+          stdEnv.mkDerivation rec {
             pname = "golden_cpp";
             version = "0.0.1";
-          
-            src = self;          
-            buildInputs = [ boost ];
-          
+
+            src = self;
+            buildInputs = [ boost17x ];
+
             nativeBuildInputs = [ catch2 cmake gnumake ninja ];
-          
+
             cmakeFlags = [
               "-DCMAKE_BUILD_TYPE=Release"
               "-DPROJECT_TESTS=On"
@@ -32,15 +36,45 @@
             hardeningEnable = [ "format" "fortify" "pic" ];
             ninjaFlags = [ "-v" ];
             makeFlags = [ "VERBOSE=1" ];
-          
+
             enableParallelBuilding = true;
             enableParallelChecking = true;
-          
+
             doCheck = true;
           });
         dev = (with pkgs;
           golden_cpp.overrideAttrs (oldAttrs: rec {
-            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+            nativeBuildInputs = let
+
+              vscodeExt = vscode-with-extensions.override {
+                # When the extension is already available in the default extensions set.
+                vscodeExtensions = with vscode-extensions;
+                  [
+		  bbenoist.Nix
+                  ms-vscode.cpptools
+]
+                  # Concise version from the vscode market place when not available in the default set.
+                  ++ vscode-utils.extensionsFromVscodeMarketplace [
+		    { name = "cmake-tools";
+                      publisher = "ms-vscode";
+                      version = "1.7.3";
+                      sha256 =
+"6UJSJETKHTx1YOvDugQO194m60Rv3UWDS8UXW6aXOko=";}
+		    { name = "emacs-mcx";
+                      publisher = "tuttieee";
+                      version = "0.31.0";
+		      sha256 = "McSWrOSYM3sMtZt48iStiUvfAXURGk16CHKfBHKj5Zk=";}
+		    { name = "cmake";
+                      publisher = "twxs";
+                      version = "0.0.17";
+		      sha256 = "CFiva1AO/oHpszbpd7lLtDzbv1Yi55yQOQPP/kCTH4Y=";}
+
+		  ];
+              };
+
+            in [
+	      # stdenv.cc.cc
+	      # libcxxabi	      
               cacert
               clang-tools
               cmakeCurses
@@ -48,9 +82,9 @@
               pkg-config
               hugo
               emacs-nox
-              vscode
               nixfmt
-            ];
+              vscodeExt
+            ] ++ oldAttrs.nativeBuildInputs;
             shellHook = ''
               export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
             '';
