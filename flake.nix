7cd70a60eccae8b8f6a3c37008d060372a24e789
nokx <72279573+nokx5@users.noreply.github.com>
2021-07-19 10:47:58 +0200

    less is cleaner

--- flake.nix
+++ flake.nix
@@ -106,7 +106,6 @@
                   self.hydraJobs.build-clang.${system}
                   #self.hydraJobs.docker.${system}
                 ] ++ lib.optionals (hostPlatform.isLinux) [
-                  self.hydraJobs.build.x86_64-linux
                   #self.hydraJobs.deb.x86_64-linux
                   #self.hydraJobs.rpm.x86_64-linux
                   #self.hydraJobs.coverage.x86_64-linux
