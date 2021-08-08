7a7a963d1148b294d46c56a7ab5b49e7bbd2aead
nokx <72279573+nokx5@users.noreply.github.com>
2021-08-08 22:26:18 +1000

    remove warning unused variable

--- src/silver/cli/silver.cpp
+++ src/silver/cli/silver.cpp
@@ -1,7 +1,10 @@
 #include "golden/include/gold.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main() {
   std::cout << "Welcome to nokx cpp silver cli!" << std::endl;
+  std::cout << std::endl
+            << "The silver executable does not provide command line parsing.."
+            << std::endl;
   return 0;
 }
